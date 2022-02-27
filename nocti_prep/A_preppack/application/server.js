47e608a00a480ab56067a129c6f7f13e2c9760c0
Leniece <71403335+LenieceB1@users.noreply.github.com>
2022-02-27 12:57:26 -0500

    Editing

--- nocti_prep/A_preppack/application/server.js
+++ nocti_prep/A_preppack/application/server.js
@@ -4,104 +4,111 @@ const fs = require('fs');
 const port = 8080
 const ip = '127.0.0.1'
 app.set('view engine', 'ejs')
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({
+  extended: true
+}))
 
 
 
 
-app.get('/', function(req,res){
+app.get('/', function(req, res) {
   res.render('index.ejs')
 })
 
 
 
-app.get('/neworder', function(req,res){
+app.get('/neworder', function(req, res) {
   res.render('neworder.ejs', {
-    error:"",
+    error: "",
     alert: ""
   })
 })
 
 
 
-app.post('/neworder', function(req,res){
+app.post('/neworder', function(req, res) {
   const name = req.body.cName
   const address = req.body.cAddress
-  const user = {
-    usrName: name,
-    usrAddress: address
-    }
-    if(name == "" || address == ""){
-      res.render('neworder.ejs', {
-        error: "Must Fill Out All Fields"
-      })
-    }
-
-    const order = {
-      orderNumber:  ,
-      order:1 ,
-      customerName: name ,
-      customerAddress: address,
+  if (name && address) {
+    var orderdata = JSON.parse(fs.readFileSync('data.json'));
+    var order = {
+      orderNumber: data.orders.length,
+      customerName: req.body.cname,
+      customerAddress: req.body.caddress,
       item: [],
       subtotal: 0,
       tax: 0,
       total: 0
     }
-
-    console.log(order);
-    outData = JSON.stringify(order)
-    fs.writeFileSync('data.json', outData, function(){
-      console.log("Data Saved: ");
+  }
+  data.neworders.push(order)
+  fs.writeFile('data.json', JSON.stringify(orderdata), function(err) {})
+  const user = {
+    usrName: name,
+    usrAddress: address
+  }
+  if (name == "" || address == "") {
+    res.render('neworder.ejs', {
+      error: "Must Fill Out All Fields"
     })
   }
-)
 
 
-app.get('/additem', function(req,res){
-  res.render('additem.ejs', {
-    error:""
+
+  console.log(order);
+  outData = JSON.stringify(order)
+  fs.writeFileSync('data.json', outData, function() {
+    console.log("Data Saved: ");
   })
 })
 
-app.post('/additem?', function(req,res){
-  var data = {
-    orderNumber: req.body.orderNumber,
-    itemName: req.body.itemName,
-    quantity: req.body.quantity,
-    price: req.body.price,
-  }
-  console.log(data.orderNumber)
 
-  if (data.orderNumber == "" || data.itemName == "" || data.quantity == "" || data.price == "") {
-    res.render('additem.ejs', {
-      error: "One or more fields has been left empty: "
-    })
-  }
-  if (data.orderNumber != '' && data.itemName != '' && data.quantity != '' && data.price != '') {
-    let rawInfo = fs.readFileSync(__dirname + '/data.json')
-    let feed = JSON.parse(rawInfo)
-    feed.comments.push(data)
-    console.log(feed)
-    fs.writeFile(__dirname + '/data.json', JSON.stringify(feed), 'utf8', function() {
-      console.log('wrote to file')
-    })
+app.get('/additem', function(req, res) {
+  res.render('additem.ejs', {
+    error: ""
+  })
 })
 
+app.post('/additem?', function(req, res) {
+      var data = {
+        orderNumber: req.body.orderNumber,
+        itemName: req.body.itemName,
+        quantity: req.body.quantity,
+        price: req.body.price,
+      }
+      console.log(data.orderNumber)
 
-app.get('/view', function(req,res){
-  res.locals.query = req.query;
-  res.render('view.ejs')
-  if(res.locals.query){
-     //how to put shit here and yeet it to view.ejs
-     //wait this isnt google
-    console.log(req.query);
-  }else{
-    console.log('no') //lol stupid this isnt gonna work, the parmaters are still there its just empty
-  }
-})
+      if (data.orderNumber == "" || data.itemName == "" || data.quantity == "" || data.price == "") {
+        res.render('additem.ejs', {
+          error: "One or more fields has been left empty: "
+        })
+      }
+      if (data.orderNumber != '' && data.itemName != '' && data.quantity != '' && data.price != '') {
+        let rawInfo = fs.readFileSync(__dirname + '/data.json')
+        let feed = JSON.parse(rawInfo)
+        feed.comments.push(data)
+        console.log(feed)
+        fs.writeFile(__dirname + '/data.json', JSON.stringify(feed), 'utf8', function() {
+          console.log('wrote to file')
+        })
+      }
 
 
+      app.get('/view', function(req, res) {
+            if (req.body.cname && req.body.cAddress) {
+              var orderdata = JSON.parse(fs.readFileSync('data.json'));
+              var order = {
+                orderNumber: data.orders.length,
+                customerName: req.body.cname,
+                customerAddress: req.body.caddress,
+                item: [],
+                subtotal: 0,
+                tax: 0,
+                total: 0
+              }
+            }
 
-app.listen(port, ip, function(){
-  console.log("Server running at 127.0.0.1:8080");
-})
+
+        app.listen(port, ip, function() {
+         console.log("Server running at 127.0.0.1:8080");
+        });
